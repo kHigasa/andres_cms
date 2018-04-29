117bcc739cd4c7d7dbe955fedfd538bf4ff38c17
kHigasa <kojihigasa@gmail.com>
2018-04-29 14:54:42 +0900

    Setting log output

--- config/application.rb
+++ config/application.rb
@@ -20,15 +20,16 @@ module AndresCms
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+
     # Initialize language
     config.i18n.default_locale = :ja
     # Setting log
     config.logger = Logger.new(STDOUT)
     config.log_formatter = ::Logger::Formatter.new
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
 
     # Don't generate system test files.
     config.generators.system_tests = nil
