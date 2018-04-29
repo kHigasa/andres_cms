4d32d10e22e3da7b7280965b27da8ff50be2e2b1
kHigasa <kojihigasa@gmail.com>
2018-04-29 14:42:55 +0900

    Setting log output

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,9 @@ module AndresCms
     config.load_defaults 5.1
     # Initialize language
     config.i18n.default_locale = :ja
+    # Setting log
+    config.logger = Logger.new(STDOUT)
+    config.log_formatter = ::Logger::Formatter.new
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
