e88b0cb47d274fb31aa1a1ad3dfceb6ae26a24ef
kHigasa <kojihigasa@gmail.com>
2018-05-05 14:33:04 +0900

    Add suer_id index to social_account

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180505052639) do
+ActiveRecord::Schema.define(version: 20180505053212) do
 
   create_table "item_images", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "image"
@@ -48,6 +48,8 @@ ActiveRecord::Schema.define(version: 20180505052639) do
     t.string "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_social_accounts_on_user_id"
   end
 
   create_table "tags", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
