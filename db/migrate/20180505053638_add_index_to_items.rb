43d9000a148d7a8976a5e4e72978396e1e42c4e6
kHigasa <kojihigasa@gmail.com>
2018-05-05 14:37:19 +0900

    Add post_id target_id index to item

---
+++ db/migrate/20180505053638_add_index_to_items.rb
@@ -0,0 +1,8 @@
+class AddIndexToItems < ActiveRecord::Migration[5.1]
+  def change
+    add_column :items, :post_id, :integer
+    add_index :items, :post_id
+    add_column :items, :target_id, :integer
+    add_index :items, :target_id
+  end
+end
