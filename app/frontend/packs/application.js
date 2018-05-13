510ebbeb7b2354424653e2e3b54ccaeb01801f7f
kHigasa <kojihigasa@gmail.com>
2018-05-13 18:40:52 +0900

    Add stationary

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -1,3 +1,4 @@
 import './application.css';
 import './pages';
+import './posts';
 import './reset.css';
---
+++ app/frontend/posts/components/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => (
+  <div className='App'>
+    <h1 className='App-title'>Hola andres!!</h1>
+  </div>
+);
+
+export default App;
