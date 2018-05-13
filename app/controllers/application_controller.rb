05fdfa130b0d695974b3d58503bf0b2a90a6faba
kHigasa <kojihigasa@gmail.com>
2018-05-13 18:30:50 +0900

    Modify after_signed_in_path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for(_user)
-    pages_show_path
+    root_path
   end
 
   def after_sign_out_path_for(_user)
