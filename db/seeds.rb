a27c48077e48b25e72bb2606ff746cd5d177fd4a
kHigasa <kojihigasa@gmail.com>
2018-05-19 22:16:41 +0900

    Hide post seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 # User
 load Rails.root.join('db', 'seeds', 'user_sd.rb')
 # Post
-load Rails.root.join('db', 'seeds', 'post_sd.rb')
+# load Rails.root.join('db', 'seeds', 'post_sd.rb')
 
 Rails.logger.info 'Creating seeds data has completed!!'
