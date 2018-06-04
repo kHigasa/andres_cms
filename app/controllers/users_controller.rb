98415b20d00aba406b80538b702e1abcfc84473c
kHigasa <kojihigasa@gmail.com>
2018-06-04 23:46:23 +0900

    Modify path argument

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only: %i[new edit create update destroy]
   before_action :set_user, only: %i[show edit update destroy]
   # GET /users
   def index
