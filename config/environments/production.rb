f2b5ad415baab16b3b5e6744e9a16e19b1580f17
kHigasa <kojihigasa@gmail.com>
2018-06-20 23:48:59 +0900

    Compile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,4 +97,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Production depends on precompilation of packs prior to booting for performance.
+  config.assets.compile = false
 end
