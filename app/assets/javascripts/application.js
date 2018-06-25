ff6f606fd794ff5a9d7cf9e347206cd9fb9b02f9
kHigasa <kojihigasa@gmail.com>
2018-06-25 17:02:42 +0900

    Modify front

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
-//= require react_ujs
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require data-confirm-modal
+//= require rails-ujs
+//= require react_ujs
 //= require_tree .
