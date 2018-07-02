348990c29697de889f75fef19faa88ecc47a3a4f
kHigasa <kojihigasa@gmail.com>
2018-07-02 23:34:41 +0900

    Wip

--- app/frontend/layouts/App.jsx
+++ app/frontend/layouts/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import Layouts from './Layouts';
 
 const app = () => (
-  <Layouts />
+  <Router>
+    <Layouts />
+  </Router>
 );
 
 export default app;
--- app/frontend/layouts/components/Navigation/NavigationItems/NavigationItems.jsx
+++ app/frontend/layouts/components/Navigation/NavigationItems/NavigationItems.jsx
@@ -1,20 +1,17 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
 
 import NavigationItem from './NavigationItem/NavigationItem';
 
 const navigationItems = () => (
-  <Router>
-    <ul className='NavigationItems'>
-      <NavigationItem link='/' exact>Home</NavigationItem>
-      <NavigationItem link='/about' exact>About</NavigationItem>
-      <NavigationItem link='/posts' exact>Posts</NavigationItem>
-      <NavigationItem link='/histories' exact>Histories</NavigationItem>
-      <NavigationItem link='/supporters' exact>Supporters</NavigationItem>
-      <NavigationItem link='/contact' exact>Contact</NavigationItem>
-      <NavigationItem link='/faq' exact>FAQ</NavigationItem>
-    </ul>
-  </Router>
+  <ul className='NavigationItems'>
+    <NavigationItem link='/' exact>Home</NavigationItem>
+    <NavigationItem link='/about' exact>About</NavigationItem>
+    <NavigationItem link='/posts' exact>Posts</NavigationItem>
+    <NavigationItem link='/histories' exact>Histories</NavigationItem>
+    <NavigationItem link='/supporters' exact>Supporters</NavigationItem>
+    <NavigationItem link='/contact' exact>Contact</NavigationItem>
+    <NavigationItem link='/faq' exact>FAQ</NavigationItem>
+  </ul>
 );
 
 export default navigationItems;
