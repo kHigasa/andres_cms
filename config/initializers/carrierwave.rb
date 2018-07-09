7a499975735ef546cc06c213938bd7487a28ddd8
kHigasa <kojihigasa@gmail.com>
2018-07-09 18:27:38 +0900

    Modify key

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,11 +2,12 @@ CarrierWave.configure do |config|
   config.fog_credentials = {
     provider: 'AWS',
     aws_access_key_id: 'AKIAI5QGWJYRRGBQ436A',
-    aws_secret_access_key: '=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',
+    aws_secret_access_key: '+YswQfYd8xZXxsFEWyRroSXOFfVgOHdz3GswLd7o',
     region: 'ap-northeast-1'
   }
 
   config.fog_directory = 'andrescms'
   config.cache_storage = :fog
   config.asset_host = 'https://s3.amazonaws.com/andrescms'
+  config.cache_dir = "#{Rails.root}/tmp/uploads"
 end
