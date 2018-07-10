2254ba1618cd23a79686497c45915b80b27be512
kHigasa <kojihigasa@gmail.com>
2018-07-10 20:00:31 +0900

    Modify some configs

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,3 @@
 Sidekiq.configure_server do |config|
-  config.redis = if Rails.env.production?
-                   { url: $redis, namespace: 'sidekiq' }
-                 elsif Rails.env.staging?
-                   { url: $redis, namespace: 'sidekiq' }
-                 else
-                   { url: 'redis://localhost:6379', namespace: 'sidekiq' }
-                 end
+  config.redis = { url: 'redis://localhost:6379', namespace: 'sidekiq' }
 end
