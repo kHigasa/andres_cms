7e432485d726a503743728f2317e5d35383abfa0
kHigasa <kojihigasa@gmail.com>
2018-07-10 19:21:30 +0900

    Setup redis

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,3 +1,9 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://localhost:6379', namespace: 'sidekiq' }
+  config.redis = if Rails.env.production?
+                   { url: 'redis://', namespace: 'sidekiq' }
+                 elsif Rails.env.staging?
+                   { url: 'redis://rediscloud:wYLFiSgQ27rdgke7PXVRSxraxSDX7BFL@redis-14632.c12.us-east-1-4.ec2.cloud.redislabs.com:14632', namespace: 'sidekiq' }
+                 else
+                   { url: 'redis://localhost:6379', namespace: 'sidekiq' }
+                 end
 end
